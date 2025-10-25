f0e412efb6692efb7a7b7e5d5025d8649bae0cbf
158BayLoL <noodntb@gmail.com>
2025-10-25 19:36:36 +0700

    project

---
+++ JS/data.js
@@ -0,0 +1,101 @@
+const ShopItemsData = [
+  
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 5000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+  {
+    id: "1",
+    name: "Nước khoáng thiên nhiên Lavie 500ml",
+    price: 780000,
+    image: "../img/Nước khoáng thiên nhiên Lavie 500ml.png",
+  },
+
+
+];
+
+function convertVND(money) {
+  var x = money;
+  x = x.toLocaleString("it-IT", { style: "currency", currency: "VND" });
+  return x;
+}
